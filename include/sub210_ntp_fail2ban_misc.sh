85aafc281868b2739166ebfcaa01cc848ab17fcf
Daniel Convissor <danielc@analysisandsolutions.com>
2016-04-02 10:34:11 -0400

    Add ctags and vim settings

--- include/sub210_ntp_fail2ban_misc.sh
+++ include/sub210_ntp_fail2ban_misc.sh
@@ -93,3 +93,65 @@ apt-get -qq -y install \
 cd /etc && git add --all && commit_if_needed "$step"
 
 ask_to_proceed "$step"
+
+
+# CTAGS AND GIT HOOKS =====================================
+
+step="universal ctags and related git hooks"
+step_header "$step"
+
+mkdir -p "$source_dir"
+
+cd "$source_dir"
+git clone https://github.com/universal-ctags/ctags.git universal-ctags
+cd universal-ctags
+./autogen.sh
+./configure
+make
+make install
+
+# Ensure the template directory is there.
+mkdir -p /usr/share/git-core/templates/hooks
+
+# Put our hooks in Git's default template directory.
+# Then, whenever git init or clone are called, these files get copied into the new
+# repository's hooks directory.
+cp "$repo_dir/git-hooks/"* /usr/share/git-core/templates/hooks
+chmod 755 /usr/share/git-core/templates/hooks/*
+
+# Make calling "git ctags" execute our ctags script.
+git config --system alias.ctags '!.git/hooks/ctags'
+
+# Obtain and install my Ctags for SVN script.
+cd "$source_dir"
+git clone git://github.com/convissor/ctags_for_svn
+ln -s "$source_dir/ctags_for_svn/ctags_for_svn.sh" "$bin_dir/ctags_for_svn.sh"
+
+cd /etc && git add --all && commit_if_needed "$step"
+ask_to_proceed "$step"
+
+
+# VIM SETTINGS ========================================
+
+cd
+if [[ ! -d vim-settings ]] ; then
+	git clone git://github.com/convissor/vim-settings.git
+	cd vim-settings
+else
+	cd vim-settings
+	# Ensure files have right permissions in case copied via thumb drive.
+	git reset --hard HEAD
+fi
+
+if [[ ! -e ~/.vimrc ]] ; then
+	./setup.sh
+fi
+
+cd
+if [[ ! -e /etc/skel/vim-settings ]] ; then
+	cp -R vim-settings /etc/skel
+	cd /etc/skel
+	ln -s vim-settings/.vim .vim
+	ln -s vim-settings/.vimrc .vimrc
+	echo "EDITOR=/usr/bin/vim" >> .profile
+fi
